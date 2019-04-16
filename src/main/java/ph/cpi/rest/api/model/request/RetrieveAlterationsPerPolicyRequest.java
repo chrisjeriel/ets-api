5d201f99b906b2c5371317c5513bc14678e8ebd0
Bryan <m.bryanmartinez@gmail.com>
2019-04-16 17:16:32 +0800

    savepoint for create alteration

---
+++ src/main/java/ph/cpi/rest/api/model/request/RetrieveAlterationsPerPolicyRequest.java
@@ -0,0 +1,35 @@
+package ph.cpi.rest.api.model.request;
+
+import ph.cpi.rest.api.model.PaginationRequest;
+import ph.cpi.rest.api.model.SortRequest;
+
+public class RetrieveAlterationsPerPolicyRequest {
+	
+	private Integer policyId;
+	private PaginationRequest paginationRequest;
+	private SortRequest sortRequest;
+	
+	public Integer getPolicyId() {
+		return policyId;
+	}
+	public void setPolicyId(Integer policyId) {
+		this.policyId = policyId;
+	}
+	public PaginationRequest getPaginationRequest() {
+		return paginationRequest;
+	}
+	public void setPaginationRequest(PaginationRequest paginationRequest) {
+		this.paginationRequest = paginationRequest;
+	}
+	public SortRequest getSortRequest() {
+		return sortRequest;
+	}
+	public void setSortRequest(SortRequest sortRequest) {
+		this.sortRequest = sortRequest;
+	}
+	@Override
+	public String toString() {
+		return "RetrieveAlterationsPerPolicyRequest [policyId=" + policyId + ", paginationRequest=" + paginationRequest
+				+ ", sortRequest=" + sortRequest + "]";
+	}
+}
