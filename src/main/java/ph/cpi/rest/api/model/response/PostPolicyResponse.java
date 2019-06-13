df301eae0e3ec4b34a977846ffdfb890e5c7dee0
Christopher <chrisjeriel@gmail.com>
2019-06-13 15:51:14 +0800

    Changed value of some to Double.

--- src/main/java/ph/cpi/rest/api/model/response/PostPolicyResponse.java
+++ src/main/java/ph/cpi/rest/api/model/response/PostPolicyResponse.java
@@ -1,5 +1,6 @@
 package ph.cpi.rest.api.model.response;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ph.cpi.rest.api.model.Error;
@@ -9,13 +10,20 @@ public class PostPolicyResponse {
 	private List<Error> errorList;
 	private List<Message> messageList;
 	private Integer returnCode;
+	
 	public List<Error> getErrorList() {
+		if (errorList == null) {
+			errorList = new ArrayList<Error>();
+		}
 		return errorList;
 	}
 	public void setErrorList(List<Error> errorList) {
 		this.errorList = errorList;
 	}
 	public List<Message> getMessageList() {
+		if (messageList == null) {
+			messageList = new ArrayList<Message>();
+		}
 		return messageList;
 	}
 	public void setMessageList(List<Message> messageList) {
