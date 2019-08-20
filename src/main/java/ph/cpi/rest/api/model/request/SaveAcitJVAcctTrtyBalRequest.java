f9438dbdf740599a937c1b4d3574b6c8813a58bc
EarlArcebido <33819853+EarlArcebido@users.noreply.github.com>
2019-08-20 19:29:51 +0800

    Savepoint for Demo

--- src/main/java/ph/cpi/rest/api/model/request/SaveAcitJVAcctTrtyBalRequest.java
+++ src/main/java/ph/cpi/rest/api/model/request/SaveAcitJVAcctTrtyBalRequest.java
@@ -69,8 +69,8 @@ class acctTreaty {
 	private BigDecimal riCommVat;
 	private BigDecimal charges;
 	private BigDecimal netDue;
-	private BigDecimal prevPaytAmt;
-	private BigDecimal balPaytAmt;
+	private BigDecimal cumPayment;
+	private BigDecimal paytAmt;
 	private BigDecimal localAmt;
 	private BigDecimal overdueInt;
 	private String remarks;
@@ -78,184 +78,143 @@ class acctTreaty {
 	private String createDate;
 	private String updateUser;
 	private String updateDate;
-	
 	public Integer getTranId() {
 		return tranId;
 	}
-
 	public void setTranId(Integer tranId) {
 		this.tranId = tranId;
 	}
-
 	public Integer getQuarterNo() {
 		return quarterNo;
 	}
-
 	public void setQuarterNo(Integer quarterNo) {
 		this.quarterNo = quarterNo;
 	}
-
 	public Integer getItemNo() {
 		return itemNo;
 	}
-
 	public void setItemNo(Integer itemNo) {
 		this.itemNo = itemNo;
 	}
-
 	public Integer getPolicyId() {
 		return policyId;
 	}
-
 	public void setPolicyId(Integer policyId) {
 		this.policyId = policyId;
 	}
-
 	public Integer getInstNo() {
 		return instNo;
 	}
-
 	public void setInstNo(Integer instNo) {
 		this.instNo = instNo;
 	}
-
 	public String getCurrCd() {
 		return currCd;
 	}
-
 	public void setCurrCd(String currCd) {
 		this.currCd = currCd;
 	}
-
 	public BigDecimal getCurrRate() {
 		return currRate;
 	}
-
 	public void setCurrRate(BigDecimal currRate) {
 		this.currRate = currRate;
 	}
-
 	public BigDecimal getPremAmt() {
 		return premAmt;
 	}
-
 	public void setPremAmt(BigDecimal premAmt) {
 		this.premAmt = premAmt;
 	}
-
 	public BigDecimal getRiComm() {
 		return riComm;
 	}
-
 	public void setRiComm(BigDecimal riComm) {
 		this.riComm = riComm;
 	}
-
 	public BigDecimal getRiCommVat() {
 		return riCommVat;
 	}
-
 	public void setRiCommVat(BigDecimal riCommVat) {
 		this.riCommVat = riCommVat;
 	}
-
 	public BigDecimal getCharges() {
 		return charges;
 	}
-
 	public void setCharges(BigDecimal charges) {
 		this.charges = charges;
 	}
-
 	public BigDecimal getNetDue() {
 		return netDue;
 	}
-
 	public void setNetDue(BigDecimal netDue) {
 		this.netDue = netDue;
 	}
-
-	public BigDecimal getPrevPaytAmt() {
-		return prevPaytAmt;
+	public BigDecimal getPaytAmt() {
+		return paytAmt;
 	}
-
-	public void setPrevPaytAmt(BigDecimal prevPaytAmt) {
-		this.prevPaytAmt = prevPaytAmt;
+	public void setPaytAmt(BigDecimal paytAmt) {
+		this.paytAmt = paytAmt;
 	}
-
-	public BigDecimal getBalPaytAmt() {
-		return balPaytAmt;
+	
+	public BigDecimal getCumPayment() {
+		return cumPayment;
 	}
-
-	public void setBalPaytAmt(BigDecimal balPaytAmt) {
-		this.balPaytAmt = balPaytAmt;
+	public void setCumPayment(BigDecimal cumPayment) {
+		this.cumPayment = cumPayment;
 	}
-
 	public BigDecimal getLocalAmt() {
 		return localAmt;
 	}
-
 	public void setLocalAmt(BigDecimal localAmt) {
 		this.localAmt = localAmt;
 	}
-
 	public BigDecimal getOverdueInt() {
 		return overdueInt;
 	}
-
 	public void setOverdueInt(BigDecimal overdueInt) {
 		this.overdueInt = overdueInt;
 	}
-
 	public String getRemarks() {
 		return remarks;
 	}
-
 	public void setRemarks(String remarks) {
 		this.remarks = remarks;
 	}
-
 	public String getCreateUser() {
 		return createUser;
 	}
-
 	public void setCreateUser(String createUser) {
 		this.createUser = createUser;
 	}
-
 	public String getCreateDate() {
 		return createDate;
 	}
-
 	public void setCreateDate(String createDate) {
 		this.createDate = createDate;
 	}
-
 	public String getUpdateUser() {
 		return updateUser;
 	}
-
 	public void setUpdateUser(String updateUser) {
 		this.updateUser = updateUser;
 	}
-
 	public String getUpdateDate() {
 		return updateDate;
 	}
-
 	public void setUpdateDate(String updateDate) {
 		this.updateDate = updateDate;
 	}
-
 	@Override
 	public String toString() {
 		return "acctTreaty [tranId=" + tranId + ", quarterNo=" + quarterNo + ", itemNo=" + itemNo + ", policyId="
 				+ policyId + ", instNo=" + instNo + ", currCd=" + currCd + ", currRate=" + currRate + ", premAmt="
 				+ premAmt + ", riComm=" + riComm + ", riCommVat=" + riCommVat + ", charges=" + charges + ", netDue="
-				+ netDue + ", prevPaytAmt=" + prevPaytAmt + ", balPaytAmt=" + balPaytAmt + ", localAmt=" + localAmt
+				+ netDue + ", cumPayment=" + cumPayment + ", paytAmt=" + paytAmt + ", localAmt=" + localAmt
 				+ ", overdueInt=" + overdueInt + ", remarks=" + remarks + ", createUser=" + createUser + ", createDate="
 				+ createDate + ", updateUser=" + updateUser + ", updateDate=" + updateDate + "]";
 	}
+	
 }
 
 
