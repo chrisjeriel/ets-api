17805331c737dc0ce3eb0faa7dfe5103317cccf1
arneillepascual <yelepascual17@gmail.com>
2019-09-25 13:27:02 +0800

    savepoint

--- src/main/java/ph/cpi/rest/api/dao/impl/AccountingServDaoImpl.java
+++ src/main/java/ph/cpi/rest/api/dao/impl/AccountingServDaoImpl.java
@@ -52,4 +52,12 @@ public class AccountingServDaoImpl implements AccountingServDao{
 		params.put("errorCode", errorCode);
 		return params;
 	}
+	
+	@Override
+	public HashMap<String, Object> updateAcsePaytReqStat(HashMap<String, Object> params) throws SQLException {
+		Integer errorCode = sqlSession.update("updateAcsePaytReqStat", params);
+		params.put("errorCode", errorCode);
+		return params;
+	}	
+
 }
--- src/main/java/ph/cpi/rest/api/service/AccountingServService.java
+++ src/main/java/ph/cpi/rest/api/service/AccountingServService.java
@@ -12,10 +12,14 @@ import ph.cpi.rest.api.model.response.RetrieveAcseOrListResponse;
 import ph.cpi.rest.api.model.response.RetrieveAcsePaytReqResponse;
 import ph.cpi.rest.api.model.request.SaveAcseOrTransRequest;
 import ph.cpi.rest.api.model.request.SaveAcsePaytReqRequest;
+import ph.cpi.rest.api.model.request.UpdateAcitPaytReqStatRequest;
+import ph.cpi.rest.api.model.request.UpdateAcsePaytReqStatRequest;
 import ph.cpi.rest.api.model.response.RetrieveAcseOrEntryResponse;
 import ph.cpi.rest.api.model.response.RetrieveAcseOrListResponse;
 import ph.cpi.rest.api.model.response.SaveAcseOrTransResponse;
 import ph.cpi.rest.api.model.response.SaveAcsePaytReqResponse;
+import ph.cpi.rest.api.model.response.UpdateAcitPaytReqStatResponse;
+import ph.cpi.rest.api.model.response.UpdateAcsePaytReqStatResponse;
 
 @Service
 public interface AccountingServService {
@@ -24,4 +28,5 @@ public interface AccountingServService {
 	public RetrieveAcsePaytReqResponse retrieveAcsePaytReq(RetrieveAcsePaytReqRequest racprlp ) throws SQLException;
 	public SaveAcseOrTransResponse saveOrEntry(SaveAcseOrTransRequest soetr) throws SQLException;
 	public SaveAcsePaytReqResponse saveAcsePaytReq(SaveAcsePaytReqRequest saprr) throws SQLException;
+	public UpdateAcsePaytReqStatResponse updateAcsePaytReqStat(UpdateAcsePaytReqStatRequest uaprsr) throws SQLException;
 }
